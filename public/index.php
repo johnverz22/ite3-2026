d129fc5e3f3e4ed965e057905cdb3baa5362a34b
johnverz22 <johnny.verzola@lorma.edu>
2026-03-31 13:21:35 +0800

    added missing codes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,30 @@
 <?php
-// ... keep the autoloader code from Handout 1 ...
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+spl_autoload_register(function ($class) {
+    
+    $prefix = 'App\\';
+
+    $base_dir = __DIR__ . '/../app/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return; // It's not our class, ignore it.
+    }
+
+    $relative_class = substr($class, $len);
+
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // If the file exists on the disk, require it!
+    if (file_exists($file)) {
+        require $file;
+    } else {
+        echo "Autoloader Error: Could not find file at $file";
+    }
+});
 
 use App\Core\Router;
 
